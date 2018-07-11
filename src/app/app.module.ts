7250153cba1afcd01eef23f2f808c42d6a3e2f5c
joao pedro ribeiro <jp3dr0436@gmail.com>
2018-07-11 10:08:58 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { HttpClientModule } from '@angular/common/http';
+import { AuthProvider } from '../providers/auth/auth';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +21,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +32,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import "rxjs/add/operator/map";
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  users: Observable<any>;
 
+  constructor(public navCtrl: NavController, private httpClient: HttpClient, private plt: Platform, private alertCtrl: AlertController) {
+    this.users = this.httpClient.get('https://randomuser.me/api/?results=20')
+      .map(res => res['results'])
+  }
+
+  checkPlatform() {
+    let alert = this.alertCtrl.create({
+      title: 'Platform',
+      message: 'You are running on: ' + this.plt.platforms(),
+      buttons: ['OK']
+    });
+    alert.present();
+
+    if (this.plt.is('cordova')) {
+      // Do Cordova stuff
+    } else {
+      // Do stuff inside the regular browser
+    }
   }
 
 }
